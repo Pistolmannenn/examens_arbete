60a5eb4240308f106c0af759914d2229c5f034ba
Erik <erik.gustafsson03@elev.ntig.se>
2023-05-11 09:36:05 +0200

    get history done

--- Gets/getHistory.php
+++ Gets/getHistory.php
@@ -10,11 +10,25 @@
         while($row = $result->fetch_assoc()){
             if(!empty($row["CardID"])){
                 if(!empty($row["CardAmount"])){
-                    $row = ["Datatype" => "Card add", "PersonID" => $row["PersonID"], "CardID"=> $row["CardID"], "CardAmount"=> $row["CardAmount"], "ActiveTime"=> $row["ActiveTime"]];
+                    $row = ["Datatype" => "Card add", "OwnerID" => $row["PersonID"], "CardID"=> $row["CardID"], "CardAmount"=> $row["CardAmount"], "ActiveTime"=> $row["ActiveTime"]];
                 }
                 else{
-                    $row = ["Datatype" => "Card done", "PersonID" => $row["PersonID"], "CardID"=> $row["CardID"], "ActiveTime"=> $row["ActiveTime"]];
+                    $row = ["Datatype" => "Card done", "OwnerID" => $row["PersonID"], "CardID"=> $row["CardID"], "ActiveTime"=> $row["ActiveTime"]];
                 }
+            }
+            elseif(!empty($row["DebtID"])){
+                if(!empty($row["Payment"])){
+                    $row = ["Datatype" => "Payment", "PayerID" => $row["PersonID"], "DebtID"=> $row["DebtID"], "Payment"=> $row["Payment"], "PaymentTime"=> $row["ActiveTime"]];
+                }
+                else{
+                    $row = ["Datatype" => "Debt paid", "PayerID" => $row["PersonID"], "DebtID"=> $row["DebtID"], "PaymentTime"=> $row["ActiveTime"]];
+                }
+            }
+            elseif(!empty($row["CheckID"])){
+                $row = ["Datatype" => "Check used", "OwnerID" => $row["PersonID"], "CheckID"=> $row["CheckID"], "UseTime"=> $row["ActiveTime"]];
+            }
+            elseif(!empty($row["HouseID"])){
+                $row = ["Datatype" => "House sold", "BuyerID" => $row["PersonID"], "HouseID"=> $row["HouseID"], "SellingTime"=> $row["ActiveTime"]];
             }
             $history[] = $row;
         }
